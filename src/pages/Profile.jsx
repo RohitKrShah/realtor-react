be42fe90f271a41322742714586efa13fa8f7e82
rohit <rohitkr14554@gmail.com>
2022-11-12 03:09:42 +0530

    add Edit and Delete functionality to my listing section

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -9,6 +9,7 @@ import {FcHome} from 'react-icons/fc'
 import { Link } from 'react-router-dom';
 import {
   collection,
+  deleteDoc,
   getDocs,
   orderBy,
   query,
@@ -81,6 +82,19 @@ useEffect(() => {
   }
   fetchUserListings();
 }, [auth.currentUser.uid]);
+async function onDelete(listingID) {
+  if (window.confirm("Are you sure you want to delete?")) {
+    await deleteDoc(doc(db, "listings", listingID));
+    const updatedListings = listings.filter(
+      (listing) => listing.id !== listingID
+    );
+    setListings(updatedListings);
+    toast.success("Successfully deleted the listing");
+  }
+}
+function onEdit(listingID) {
+  navigate(`/edit-listing/${listingID}`);
+}
   return (
     <>
       <section className="max-w-6xl mx-auto flex justify-center items-center flex-col">
@@ -153,6 +167,8 @@ useEffect(() => {
                   key={listing.id}
                   id={listing.id}
                   listing={listing.data}
+                  onDelete={() => onDelete(listing.id)}
+                  onEdit={() => onEdit(listing.id)}
                 />
               ))}
             </ul>
